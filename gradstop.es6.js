2b3e7bb3e6cad81ddc3bf0e2bb94c67dc0430930
Siddharth11 <siddharth2parmar@gmail.com>
2015-10-28 01:01:50 +0530

    template strings added

--- gradstop.es6.js
+++ gradstop.es6.js
@@ -1,12 +1,3 @@
-let defaultOption = {
-    // input color options: hex, rgb or hsl
-    inColor: 'hex',
-    // number of equidistant color stops (cannot be less than colorArray.length)
-    stops: 5,
-    // input color array (currently supports only 2)
-    colorArray: ['#fff', '#000']
-}
-
 /**
  * extend obj function
  */
@@ -32,7 +23,15 @@ let hexToRgb = (hex) => {
 }
 
 class gradStop {
-    constructor(options = defaultOption) {
+    constructor(options) {
+        this.options = {
+            // input color options: hex, rgb or hsl
+            inColor: 'hex',
+            // number of equidistant color stops (cannot be less than colorArray.length)
+            stops: 5,
+            // input color array (currently supports only 2)
+            colorArray: ['#fff', '#000']
+        }
         this.options = extend({}, this.options)
         extend(this.options, options)
         this._init()
@@ -44,7 +43,6 @@ class gradStop {
     // methods
     _init() {
         if (this.options.inColor === 'hex') {
-
             let fixedHexFormat = this.options.colorArray.map((color) => {
                 if (color.length === 3) {
                     color = color + color.split('').slice(1, 3).join('') + color.split('').slice(1, 3).join('')
@@ -60,20 +58,20 @@ class gradStop {
         } else if (this.options.inColor === 'rgb') {
             this.colorArray = this.options.colorArray.map((color) => {
                 color = color.split('').slice(4, -1).join('').split(',')
-                let tempObj = {}
-                tempObj.r = color[0]
-                tempObj.g = color[1]
-                tempObj.b = color[2]
-                return tempObj
+                let [r, g, b] = [color]
+                return {
+                    r, g, b
+                }
             })
         } else if (this.options.inColor === 'hsl') {
             this.colorArray = this.options.colorArray.map((color) => {
                 color = color.split('').slice(4, -1).join('').split(',')
-                let tempObj = {}
-                tempObj.h = color[0]
-                tempObj.s = color[1].split('').slice(0, -1).join('')
-                tempObj.l = color[2].split('').slice(0, -1).join('')
-                return tempObj
+                let h = color[0],
+                    s = color[1].split('').slice(0, -1).join(''),
+                    l = color[2].split('').slice(0, -1).join('')
+                return {
+                    h, s, l
+                }
             })
         }
     }
@@ -85,19 +83,19 @@ class gradStop {
         if (this.options.inColor === 'hex' || this.options.inColor === 'rgb') {
 
             // red
-            let redStart = parseInt(this.colorArray[0].r)
-            let redEnd = parseInt(this.colorArray[1].r)
-            let rIncrement = (redEnd - redStart) / (this.options.stops - 1)
+            let redStart = parseInt(this.colorArray[0].r),
+                redEnd = parseInt(this.colorArray[1].r),
+                rIncrement = (redEnd - redStart) / (this.options.stops - 1)
 
             // green
-            let greenStart = parseInt(this.colorArray[0].g)
-            let greenEnd = parseInt(this.colorArray[1].g)
-            let gIncrement = (greenEnd - greenStart) / (this.options.stops - 1)
+            let greenStart = parseInt(this.colorArray[0].g),
+                greenEnd = parseInt(this.colorArray[1].g),
+                gIncrement = (greenEnd - greenStart) / (this.options.stops - 1)
 
             // blue
-            let blueStart = parseInt(this.colorArray[0].b)
-            let blueEnd = parseInt(this.colorArray[1].b)
-            let bIncrement = (blueEnd - blueStart) / (this.options.stops - 1)
+            let blueStart = parseInt(this.colorArray[0].b),
+                blueEnd = parseInt(this.colorArray[1].b),
+                bIncrement = (blueEnd - blueStart) / (this.options.stops - 1)
 
             let i
             for (i = 0; i < this.options.stops; i++) {
@@ -105,25 +103,25 @@ class gradStop {
                 r = redStart + Math.floor(rIncrement * i)
                 g = greenStart + Math.floor(gIncrement * i)
                 b = blueStart + Math.floor(bIncrement * i)
-                this.outputArray.push('rgb(' + r + ',' + g + ',' + b + ')')
+                this.outputArray.push(`rgb(${r},${g},${b})`)
             }
 
         } else if (this.options.inColor === 'hsl') {
 
             // hue
-            let hueStart = parseInt(this.colorArray[0].h)
-            let hueEnd = parseInt(this.colorArray[1].h)
-            let hIncrement = (hueEnd - hueStart) / (this.options.stops - 1)
+            let hueStart = parseInt(this.colorArray[0].h),
+                hueEnd = parseInt(this.colorArray[1].h),
+                hIncrement = (hueEnd - hueStart) / (this.options.stops - 1)
 
             // saturation
-            let satStart = parseInt(this.colorArray[0].s)
-            let satEnd = parseInt(this.colorArray[1].s)
-            let sIncrement = (satEnd - satStart) / (this.options.stops - 1)
+            let satStart = parseInt(this.colorArray[0].s),
+                satEnd = parseInt(this.colorArray[1].s),
+                sIncrement = (satEnd - satStart) / (this.options.stops - 1)
 
             // luminance
-            let lumStart = parseInt(this.colorArray[0].l)
-            let lumEnd = parseInt(this.colorArray[1].l)
-            let lIncrement = (lumEnd - lumStart) / (this.options.stops - 1)
+            let lumStart = parseInt(this.colorArray[0].l),
+                lumEnd = parseInt(this.colorArray[1].l),
+                lIncrement = (lumEnd - lumStart) / (this.options.stops - 1)
 
             let i
             for (i = 0; i < this.options.stops; i++) {
@@ -131,7 +129,7 @@ class gradStop {
                 h = hueStart + Math.floor(hIncrement * i)
                 s = satStart + Math.floor(sIncrement * i)
                 l = lumStart + Math.floor(lIncrement * i)
-                this.outputArray.push('hsl(' + h + ',' + s + '%,' + l + '%)')
+                this.outputArray.push(`hsl(${h}, ${s}%, ${l}%)`)
             }
 
         }
--- gradstop.js
+++ gradstop.js
@@ -1,21 +1,12 @@
+/**
+ * extend obj function
+ */
 'use strict';
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-var defaultOption = {
-    // input color options: hex, rgb or hsl
-    inColor: 'hex',
-    // number of equidistant color stops (cannot be less than colorArray.length)
-    stops: 5,
-    // input color array (currently supports only 2)
-    colorArray: ['#fff', '#000']
-};
-
-/**
- * extend obj function
- */
 var extend = function extend(a, b) {
     for (var key in b) {
         if (b.hasOwnProperty(key)) {
@@ -38,11 +29,17 @@ var hexToRgb = function hexToRgb(hex) {
 };
 
 var gradStop = (function () {
-    function gradStop() {
-        var options = arguments.length <= 0 || arguments[0] === undefined ? defaultOption : arguments[0];
-
+    function gradStop(options) {
         _classCallCheck(this, gradStop);
 
+        this.options = {
+            // input color options: hex, rgb or hsl
+            inColor: 'hex',
+            // number of equidistant color stops (cannot be less than colorArray.length)
+            stops: 5,
+            // input color array (currently supports only 2)
+            colorArray: ['#fff', '#000']
+        };
         this.options = extend({}, this.options);
         extend(this.options, options);
         this._init();
@@ -57,7 +54,6 @@ var gradStop = (function () {
         key: '_init',
         value: function _init() {
             if (this.options.inColor === 'hex') {
-
                 var fixedHexFormat = this.options.colorArray.map(function (color) {
                     if (color.length === 3) {
                         color = color + color.split('').slice(1, 3).join('') + color.split('').slice(1, 3).join('');
@@ -75,20 +71,24 @@ var gradStop = (function () {
             } else if (this.options.inColor === 'rgb') {
                 this.colorArray = this.options.colorArray.map(function (color) {
                     color = color.split('').slice(4, -1).join('').split(',');
-                    var tempObj = {};
-                    tempObj.r = color[0];
-                    tempObj.g = color[1];
-                    tempObj.b = color[2];
-                    return tempObj;
+                    var _ref = [color];
+                    var r = _ref[0];
+                    var g = _ref[1];
+                    var b = _ref[2];
+
+                    return {
+                        r: r, g: g, b: b
+                    };
                 });
             } else if (this.options.inColor === 'hsl') {
                 this.colorArray = this.options.colorArray.map(function (color) {
                     color = color.split('').slice(4, -1).join('').split(',');
-                    var tempObj = {};
-                    tempObj.h = color[0];
-                    tempObj.s = color[1].split('').slice(0, -1).join('');
-                    tempObj.l = color[2].split('').slice(0, -1).join('');
-                    return tempObj;
+                    var h = color[0],
+                        s = color[1].split('').slice(0, -1).join(''),
+                        l = color[2].split('').slice(0, -1).join('');
+                    return {
+                        h: h, s: s, l: l
+                    };
                 });
             }
         }
@@ -101,19 +101,19 @@ var gradStop = (function () {
             if (this.options.inColor === 'hex' || this.options.inColor === 'rgb') {
 
                 // red
-                var redStart = parseInt(this.colorArray[0].r);
-                var redEnd = parseInt(this.colorArray[1].r);
-                var rIncrement = (redEnd - redStart) / (this.options.stops - 1);
+                var redStart = parseInt(this.colorArray[0].r),
+                    redEnd = parseInt(this.colorArray[1].r),
+                    rIncrement = (redEnd - redStart) / (this.options.stops - 1);
 
                 // green
-                var greenStart = parseInt(this.colorArray[0].g);
-                var greenEnd = parseInt(this.colorArray[1].g);
-                var gIncrement = (greenEnd - greenStart) / (this.options.stops - 1);
+                var greenStart = parseInt(this.colorArray[0].g),
+                    greenEnd = parseInt(this.colorArray[1].g),
+                    gIncrement = (greenEnd - greenStart) / (this.options.stops - 1);
 
                 // blue
-                var blueStart = parseInt(this.colorArray[0].b);
-                var blueEnd = parseInt(this.colorArray[1].b);
-                var bIncrement = (blueEnd - blueStart) / (this.options.stops - 1);
+                var blueStart = parseInt(this.colorArray[0].b),
+                    blueEnd = parseInt(this.colorArray[1].b),
+                    bIncrement = (blueEnd - blueStart) / (this.options.stops - 1);
 
                 var i = undefined;
                 for (i = 0; i < this.options.stops; i++) {
@@ -128,19 +128,19 @@ var gradStop = (function () {
             } else if (this.options.inColor === 'hsl') {
 
                 // hue
-                var hueStart = parseInt(this.colorArray[0].h);
-                var hueEnd = parseInt(this.colorArray[1].h);
-                var hIncrement = (hueEnd - hueStart) / (this.options.stops - 1);
+                var hueStart = parseInt(this.colorArray[0].h),
+                    hueEnd = parseInt(this.colorArray[1].h),
+                    hIncrement = (hueEnd - hueStart) / (this.options.stops - 1);
 
                 // saturation
-                var satStart = parseInt(this.colorArray[0].s);
-                var satEnd = parseInt(this.colorArray[1].s);
-                var sIncrement = (satEnd - satStart) / (this.options.stops - 1);
+                var satStart = parseInt(this.colorArray[0].s),
+                    satEnd = parseInt(this.colorArray[1].s),
+                    sIncrement = (satEnd - satStart) / (this.options.stops - 1);
 
                 // luminance
-                var lumStart = parseInt(this.colorArray[0].l);
-                var lumEnd = parseInt(this.colorArray[1].l);
-                var lIncrement = (lumEnd - lumStart) / (this.options.stops - 1);
+                var lumStart = parseInt(this.colorArray[0].l),
+                    lumEnd = parseInt(this.colorArray[1].l),
+                    lIncrement = (lumEnd - lumStart) / (this.options.stops - 1);
 
                 var i = undefined;
                 for (i = 0; i < this.options.stops; i++) {
@@ -150,7 +150,7 @@ var gradStop = (function () {
                     h = hueStart + Math.floor(hIncrement * i);
                     s = satStart + Math.floor(sIncrement * i);
                     l = lumStart + Math.floor(lIncrement * i);
-                    this.outputArray.push('hsl(' + h + ',' + s + '%,' + l + '%)');
+                    this.outputArray.push('hsl(' + h + ', ' + s + '%, ' + l + '%)');
                 }
             }
         }
