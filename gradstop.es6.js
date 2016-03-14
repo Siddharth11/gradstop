7520d5b932c5c7f7d3e7d95745e52f1d13fed596
Siddharth11 <siddharth2parmar@gmail.com>
2016-03-14 06:07:20 +0530

    shorthand hex bug fixed; all funcs curried

--- gradstop.es6.js
+++ gradstop.es6.js
@@ -6,12 +6,11 @@
                 throw new TypeError('Cannot convert undefined or null to object')
             }
             let output = Object(target)
-            for (let index = 1; index < arguments.length; index++) {
-                let source = arguments[index]
-                if (source !== undefined && source !== null) {
-                    for (let nextKey in source) {
-                        if (source.hasOwnProperty(nextKey)) {
-                            output[nextKey] = source[nextKey]
+            for (let arg of arguments) {
+                if (arg !== undefined && arg !== null) {
+                    for (let nextKey in arg) {
+                        if (arg.hasOwnProperty(nextKey)) {
+                            output[nextKey] = arg[nextKey]
                         }
                     }
                 }
@@ -25,9 +24,7 @@
         return this.computeStops(options)
     }
 
-    /**
-     * GradStop options
-     */
+    // GradStop options
     GradStop.prototype.options = {
         // input color options: hex, rgb or hsl
         inputFormat: 'hex',
@@ -37,35 +34,27 @@
         colorArray: ['#fff', '#000']
     }
 
-    /**
-     * computeStops
-     */
+    // computeStops
     GradStop.prototype.computeStops = options => {
 
         let outputArray = []
 
-        /**
-         * utlils
-         */
+        // utlils
         const hexToRgb = hex => {
             let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex),
                 [, r, g, b] = [...result.map(val => parseInt(val, 16))]
-            return result ? {
-                r, g, b
-            } : null
+            return result ? { r, g, b } : null
         }
 
-        const splitSliceJoin = (string, start, end) => string.split('').slice(start, end).join('')
+        const splitSliceJoin = string => start => end => string.split('').slice(start, end).join('')
 
+        // if hex and defined as #fff then convert it to standard 7 letter format #ffffff
         const init = options => {
-            // if hex and defined as #ff or #fff then convert it to standard 7 letter format #ffffff
 
             if (options.inputFormat === 'hex') {
                 let fixedHexFormat = options.colorArray.map(color => {
-                    if (color.length === 3) {
-                        return color + splitSliceJoin(color, 1, 3) + splitSliceJoin(color, 1, 3)
-                    } else if (color.length === 4) {
-                        return color + splitSliceJoin(color, 1, 4)
+                    if (color.length === 4) {
+                        return `#${color[1] + color[1] + color[2] + color[2] + color[3] + color[3]}`
                     } else if (color.length === 7) {
                         return color
                     }
@@ -75,22 +64,18 @@
             // if rgb then extract r, g anb b values
             else if (options.inputFormat === 'rgb') {
                 return options.colorArray.map(color => {
-                    let [r, g, b] = [...splitSliceJoin(color, 4, -1).split(',')]
-                    return {
-                        r, g, b
-                    }
+                    let [r, g, b] = [...splitSliceJoin(color)(4)(-1).split(',')]
+                    return { r, g, b }
                 })
             }
             // if hsl then extract h, s and l values
             else if (options.inputFormat === 'hsl') {
                 return options.colorArray.map(color => {
-                    color = splitSliceJoin(color, 4, -1).split(',')
+                    color = splitSliceJoin(color)(4)(-1).split(',')
                     let h = color[0],
-                        s = splitSliceJoin(color[1], 0, -1),
-                        l = splitSliceJoin(color[2], 0, -1)
-                    return {
-                        h, s, l
-                    }
+                        s = splitSliceJoin(color[1])(0)(-1),
+                        l = splitSliceJoin(color[2])(0)(-1)
+                    return { h, s, l }
                 })
             }
         }
@@ -100,25 +85,24 @@
             let colorArray = options.colorArray
 
             // calculate start and end values of r,g,b,h,s and l
-            const startEnd = (property) => colorArray.map(val => parseInt(val[property]))
+            const startEnd = property => colorArray.map(val => parseInt(val[property]))
 
             // calculate increment value
             const increment = (start, end) => (end - start) / (options.stops - 1)
 
             // calculate step values of r,g,b,h,s and l
-            const stepVal = (property, index) => startEnd(property)[0] + Math.trunc(increment(...startEnd(property)) * index)
+            const stepVal = property => index => startEnd(property)[0] + Math.trunc(increment(...startEnd(property)) * index)
 
             if (options.inputFormat === 'hex' || options.inputFormat === 'rgb') {
 
                 for (let i = 0; i < options.stops; i++) {
-                    let [r, g, b] = [...['r', 'g', 'b'].map(char => stepVal(char, i))]
+                    let [r, g, b] = [...['r', 'g', 'b'].map(char => stepVal(char)(i))]
                     outputArray.push(`rgb(${r},${g},${b})`)
                 }
-
             } else if (options.inputFormat === 'hsl') {
 
                 for (let i = 0; i < options.stops; i++) {
-                    let [h, s, l] = [...['h', 's', 'l'].map(char => stepVal(char, i))]
+                    let [h, s, l] = [...['h', 's', 'l'].map(char => stepVal(char)(i))]
                     outputArray.push(`hsl(${h}, ${s}%, ${l}%)`)
                 }
             }
--- gradstop.js
+++ gradstop.js
@@ -12,16 +12,37 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
                 throw new TypeError('Cannot convert undefined or null to object');
             }
             var output = Object(target);
-            for (var index = 1; index < _arguments.length; index++) {
-                var source = _arguments[index];
-                if (source !== undefined && source !== null) {
-                    for (var nextKey in source) {
-                        if (source.hasOwnProperty(nextKey)) {
-                            output[nextKey] = source[nextKey];
+            var _iteratorNormalCompletion = true;
+            var _didIteratorError = false;
+            var _iteratorError = undefined;
+
+            try {
+                for (var _iterator = _arguments[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                    var arg = _step.value;
+
+                    if (arg !== undefined && arg !== null) {
+                        for (var nextKey in arg) {
+                            if (arg.hasOwnProperty(nextKey)) {
+                                output[nextKey] = arg[nextKey];
+                            }
                         }
                     }
                 }
+            } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion && _iterator['return']) {
+                        _iterator['return']();
+                    }
+                } finally {
+                    if (_didIteratorError) {
+                        throw _iteratorError;
+                    }
+                }
             }
+
             return output;
         };
     }
@@ -31,9 +52,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
         return this.computeStops(options);
     }
 
-    /**
-     * GradStop options
-     */
+    // GradStop options
     GradStop.prototype.options = {
         // input color options: hex, rgb or hsl
         inputFormat: 'hex',
@@ -43,16 +62,12 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
         colorArray: ['#fff', '#000']
     };
 
-    /**
-     * computeStops
-     */
+    // computeStops
     GradStop.prototype.computeStops = function (options) {
 
         var outputArray = [];
 
-        /**
-         * utlils
-         */
+        // utlils
         var hexToRgb = function hexToRgb(hex) {
             var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 
@@ -64,24 +79,24 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
             var g = _ref[2];
             var b = _ref[3];
 
-            return result ? {
-                r: r, g: g, b: b
-            } : null;
+            return result ? { r: r, g: g, b: b } : null;
         };
 
-        var splitSliceJoin = function splitSliceJoin(string, start, end) {
-            return string.split('').slice(start, end).join('');
+        var splitSliceJoin = function splitSliceJoin(string) {
+            return function (start) {
+                return function (end) {
+                    return string.split('').slice(start, end).join('');
+                };
+            };
         };
 
+        // if hex and defined as #fff then convert it to standard 7 letter format #ffffff
         var init = function init(options) {
-            // if hex and defined as #ff or #fff then convert it to standard 7 letter format #ffffff
 
             if (options.inputFormat === 'hex') {
                 var fixedHexFormat = options.colorArray.map(function (color) {
-                    if (color.length === 3) {
-                        return color + splitSliceJoin(color, 1, 3) + splitSliceJoin(color, 1, 3);
-                    } else if (color.length === 4) {
-                        return color + splitSliceJoin(color, 1, 4);
+                    if (color.length === 4) {
+                        return '#' + (color[1] + color[1] + color[2] + color[2] + color[3] + color[3]);
                     } else if (color.length === 7) {
                         return color;
                     }
@@ -93,27 +108,23 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
             // if rgb then extract r, g anb b values
             else if (options.inputFormat === 'rgb') {
                     return options.colorArray.map(function (color) {
-                        var _ref2 = [].concat(_toConsumableArray(splitSliceJoin(color, 4, -1).split(',')));
+                        var _ref2 = [].concat(_toConsumableArray(splitSliceJoin(color)(4)(-1).split(',')));
 
                         var r = _ref2[0];
                         var g = _ref2[1];
                         var b = _ref2[2];
 
-                        return {
-                            r: r, g: g, b: b
-                        };
+                        return { r: r, g: g, b: b };
                     });
                 }
                 // if hsl then extract h, s and l values
                 else if (options.inputFormat === 'hsl') {
                         return options.colorArray.map(function (color) {
-                            color = splitSliceJoin(color, 4, -1).split(',');
+                            color = splitSliceJoin(color)(4)(-1).split(',');
                             var h = color[0],
-                                s = splitSliceJoin(color[1], 0, -1),
-                                l = splitSliceJoin(color[2], 0, -1);
-                            return {
-                                h: h, s: s, l: l
-                            };
+                                s = splitSliceJoin(color[1])(0)(-1),
+                                l = splitSliceJoin(color[2])(0)(-1);
+                            return { h: h, s: s, l: l };
                         });
                     }
         };
@@ -135,14 +146,16 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
             };
 
             // calculate step values of r,g,b,h,s and l
-            var stepVal = function stepVal(property, index) {
-                return startEnd(property)[0] + Math.trunc(increment.apply(undefined, _toConsumableArray(startEnd(property))) * index);
+            var stepVal = function stepVal(property) {
+                return function (index) {
+                    return startEnd(property)[0] + Math.trunc(increment.apply(undefined, _toConsumableArray(startEnd(property))) * index);
+                };
             };
 
             if (options.inputFormat === 'hex' || options.inputFormat === 'rgb') {
                 var _loop = function (i) {
                     var _ref3 = [].concat(_toConsumableArray(['r', 'g', 'b'].map(function (char) {
-                        return stepVal(char, i);
+                        return stepVal(char)(i);
                     })));
 
                     var r = _ref3[0];
@@ -158,7 +171,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
             } else if (options.inputFormat === 'hsl') {
                 var _loop2 = function (i) {
                     var _ref4 = [].concat(_toConsumableArray(['h', 's', 'l'].map(function (char) {
-                        return stepVal(char, i);
+                        return stepVal(char)(i);
                     })));
 
                     var h = _ref4[0];
